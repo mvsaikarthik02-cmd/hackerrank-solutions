d73444514dde9168da7d1cc96743ec0c82988d1b
Venkata Sai Karthik Matha <mvsaikarthik02@gmail.com>
2026-08-20 10:59:22 -0500

    ✅ Japanese Cities' Attributes [mysql] — approach1

---
+++ solutions/mysql/easy/japanese-cities-attributes/solution-approach1.sql
@@ -0,0 +1,14 @@
+-- ──────────────────────────────────────────────────
+-- Link        https://www.hackerrank.com/challenges/japanese-cities-attributes/problem?isFullScreen=true
+-- Problem     Japanese Cities' Attributes
+-- Difficulty  Easy
+-- Subdomain   Basic Select
+-- Platform    HackerRank
+-- Language    mysql
+-- Status      Accepted
+-- Submitted   2026-08-20, 10:59 a.m.
+-- ──────────────────────────────────────────────────
+
+Select *
+From CITY
+Where COUNTRYCODE='JPN';
