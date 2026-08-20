f49fc8e473ff3b02869c6496700b4f0a07b7c5ce
Venkata Sai Karthik Matha <mvsaikarthik02@gmail.com>
2026-08-20 10:56:19 -0500

    ✅ Select By ID [mysql] — approach1

---
+++ solutions/mysql/easy/select-by-id/solution-approach1.sql
@@ -0,0 +1,14 @@
+-- ──────────────────────────────────────────────────
+-- Link        https://www.hackerrank.com/challenges/select-by-id/problem?isFullScreen=true
+-- Problem     Select By ID
+-- Difficulty  Easy
+-- Subdomain   Basic Select
+-- Platform    HackerRank
+-- Language    mysql
+-- Status      Accepted
+-- Submitted   2026-08-20, 10:56 a.m.
+-- ──────────────────────────────────────────────────
+
+SELECT *
+FROM CITY
+WHERE ID=1661;
