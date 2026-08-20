9b44702db945a93be2bd3089f38eedb773667d6a
Venkata Sai Karthik Matha <mvsaikarthik02@gmail.com>
2026-08-20 10:53:38 -0500

    ✅ Revising the Select Query I [mysql] — approach1

---
+++ solutions/mysql/easy/revising-the-select-query/solution-approach1.sql
@@ -0,0 +1,14 @@
+-- ──────────────────────────────────────────────────
+-- Link        https://www.hackerrank.com/challenges/revising-the-select-query/problem?isFullScreen=true
+-- Problem     Revising the Select Query I
+-- Difficulty  Easy
+-- Subdomain   Basic Select
+-- Platform    HackerRank
+-- Language    mysql
+-- Status      Accepted
+-- Submitted   2026-08-20, 10:53 a.m.
+-- ──────────────────────────────────────────────────
+
+SElect *
+From City
+Where Population >100000 AND CountryCode='USA';
