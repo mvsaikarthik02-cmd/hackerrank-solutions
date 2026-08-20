832115b6855bd432e940ff09f25127524cb664d4
Venkata Sai Karthik Matha <mvsaikarthik02@gmail.com>
2026-08-20 11:00:23 -0500

    ✅ Japanese Cities' Names [mysql] — approach1

---
+++ solutions/mysql/easy/japanese-cities-name/solution-approach1.sql
@@ -0,0 +1,14 @@
+-- ──────────────────────────────────────────────────
+-- Link        https://www.hackerrank.com/challenges/japanese-cities-name/problem?isFullScreen=true
+-- Problem     Japanese Cities' Names
+-- Difficulty  Easy
+-- Subdomain   Basic Select
+-- Platform    HackerRank
+-- Language    mysql
+-- Status      Accepted
+-- Submitted   2026-08-20, 11:00 a.m.
+-- ──────────────────────────────────────────────────
+
+Select NAME
+From CITY
+Where COUNTRYCODE='JPN';
