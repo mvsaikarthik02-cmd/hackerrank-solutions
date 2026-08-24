2547d79449db7b70f550f509893fd76a6179c1a3
Venkata Sai Karthik Matha <mvsaikarthik02@gmail.com>
2026-08-24 12:42:08 -0500

    ✅ Population Density Difference [mysql] — approach1

---
+++ solutions/mysql/easy/population-density-difference/solution-approach1.sql
@@ -0,0 +1,13 @@
+-- ──────────────────────────────────────────────────
+-- Link        https://www.hackerrank.com/challenges/population-density-difference/problem?isFullScreen=true
+-- Problem     Population Density Difference
+-- Difficulty  Easy
+-- Subdomain   Aggregation
+-- Platform    HackerRank
+-- Language    mysql
+-- Status      Accepted
+-- Submitted   2026-08-24, 12:42 p.m.
+-- ──────────────────────────────────────────────────
+
+Select Max(Population)-Min(Population)
+From CITY;
