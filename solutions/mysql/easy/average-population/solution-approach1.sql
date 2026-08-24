65d28dc406df09c118c2db9ba212d8eb8bf8c454
Venkata Sai Karthik Matha <mvsaikarthik02@gmail.com>
2026-08-24 12:39:13 -0500

    ✅ Average Population [mysql] — approach1

---
+++ solutions/mysql/easy/average-population/solution-approach1.sql
@@ -0,0 +1,13 @@
+-- ──────────────────────────────────────────────────
+-- Link        https://www.hackerrank.com/challenges/average-population/problem?isFullScreen=true
+-- Problem     Average Population
+-- Difficulty  Easy
+-- Subdomain   Aggregation
+-- Platform    HackerRank
+-- Language    mysql
+-- Status      Accepted
+-- Submitted   2026-08-24, 12:39 p.m.
+-- ──────────────────────────────────────────────────
+
+Select FLOOR(AVG(POPULATION))
+FROM CITY;
